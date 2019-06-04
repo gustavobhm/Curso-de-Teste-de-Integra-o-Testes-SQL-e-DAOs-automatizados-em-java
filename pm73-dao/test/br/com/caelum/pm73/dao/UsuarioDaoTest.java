7203728e486fdb588ba06f37a93a93c499469ef8
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2019-06-04 10:05:35 -0300

    Aula 4 - Testando alteração e deleção

--- pm73-dao/test/br/com/caelum/pm73/dao/UsuarioDaoTest.java
+++ pm73-dao/test/br/com/caelum/pm73/dao/UsuarioDaoTest.java
@@ -1,6 +1,7 @@
 package br.com.caelum.pm73.dao;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 import org.hibernate.Session;
@@ -44,6 +45,45 @@ public class UsuarioDaoTest {
 		Usuario usuarioDoBanco = usuarioDao.porNomeEEmail("João Joaquim", "joao@joaquim.com.br");
 
 		assertNull(usuarioDoBanco);
+	}
+
+	@Test
+	public void deveDeletarUmUsuario() {
+		Usuario usuario = new Usuario("Mauricio Aniche", "mauricio@aniche.com.br");
+
+		usuarioDao.salvar(usuario);
+		usuarioDao.deletar(usuario);
+
+		// envia tudo para o banco de dados
+		session.flush();
+		session.clear();
+
+		Usuario usuarioNoBanco = usuarioDao.porNomeEEmail("Mauricio Aniche", "mauricio@aniche.com.br");
+
+		assertNull(usuarioNoBanco);
+
+	}
+
+	@Test
+	public void deveAlterarUmUsuario() {
+		Usuario usuario = new Usuario("Mauricio Aniche", "mauricio@aniche.com.br");
+
+		usuarioDao.salvar(usuario);
+
+		usuario.setNome("João da Silva");
+		usuario.setEmail("joao@silva.com.br");
+
+		usuarioDao.atualizar(usuario);
+
+		session.flush();
+
+		Usuario novoUsuario = usuarioDao.porNomeEEmail("João da Silva", "joao@silva.com.br");
+		assertNotNull(novoUsuario);
+		System.out.println(novoUsuario);
+
+		Usuario usuarioInexistente = usuarioDao.porNomeEEmail("Mauricio Aniche", "mauricio@aniche.com.br");
+		assertNull(usuarioInexistente);
+
 	}
 
 }
